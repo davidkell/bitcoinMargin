018d98a0c3d49811d1f83dfa62fc322e46135ff6
Keller Scholl <keller.scholl@gmail.com>
2015-02-28 16:32:55 +0000

    Updated prediction.py with further details.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# bitcoinMargin
-How many bitcoins do you need to have to have a certain amount of value in bitcoins later?
+bitcoinMargin
+How many bitcoins do you need to have to have a certain amount of value in bitcoins later? This website aims to answer this question for you simply and easily. 
--- prediction.py
+++ prediction.py
@@ -1,10 +1,13 @@
 def convertData(data):
     pass
-class Model:
+class Model: 
+"""A single mathematical model of the past data. 
+"""
 	def __init__(self, f):
 		self.func = f
-	def p(self,true,predicted):
 		
+	def p(self,val):
+	
 def make_models(data_subset):
 	pass
 def trainModels(models, data_subset):
