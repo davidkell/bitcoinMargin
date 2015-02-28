9ad61d068a1b56dd202b6e4f08e9a01593fb6d3b
Keller Scholl <keller.scholl@gmail.com>
2015-02-28 19:48:21 +0000

    added a bit more to prediction.py.

--- prediction.py
+++ prediction.py
@@ -1,7 +1,7 @@
 def getClosingPrices(data):
 	return [i['PX_LAST'] for i in data]
 class Model: 
-"""A single mathematical model of the past data.
+	"""A single mathematical model of the past data.
 
 	To initiate a model, give a list pvals. pvals should be the 
 	probability of a given result, for every possible result 
@@ -10,8 +10,7 @@ class Model:
 		(small error margin included to account for floating points.)
 	Alternatively, instead of providing the p-values explicitly, one
 	may instead 
-
-"""
+	"""
 	def __init__(self, pvals=[],func=lambda:(0)):
 		try:
 			assert sum(pvals) >.99
@@ -26,8 +25,13 @@ class Model:
 		if self.discrete:
 			return self.pvals[val]
 		return self.func(val)
+	def confidence_value_below()
+		pass
 
 def make_models(data_subset):
 	pass
+	#Look at data in different resolutions.
+	#Check out weekend effects, check out 
+	#Consider other bloomberg 
 def trainModels(models, data_subset):
 	pass
